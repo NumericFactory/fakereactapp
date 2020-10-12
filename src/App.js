201961c999c65b49cf168f50cae08997e45d7811
NumericFactory <frederic.lossignol@gmail.com>
2020-10-12 15:17:47 +0200

    hello

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Editezzzzzz <code>src/App.js</code> and save to reload.
+          Editezooooooooo <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
